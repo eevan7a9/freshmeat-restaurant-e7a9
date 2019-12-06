646fb286bbfd6436d41d885ed338df6b3d01c586
eevan7a9 <e7a9@protonmail.com>
2019-12-06 19:06:57 +0900

    added scss

---
+++ frontend-restaurant/src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link>|
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
+<style lang="scss" scoped>
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+  }
+  a.router-link-exact-active {
+    color: #42b983;
+  }
+}
+</style>
