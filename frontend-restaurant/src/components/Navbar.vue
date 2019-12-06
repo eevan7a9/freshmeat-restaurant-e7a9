1762feccdbb269acb092bc73fbe07e2e8fe8f210
eevan7a9 <e7a9@protonmail.com>
2019-12-06 21:04:16 +0900

    added navigation bar

--- frontend-restaurant/src/components/Navbar.vue
+++ frontend-restaurant/src/components/Navbar.vue
@@ -1,23 +1,169 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>|
-    <router-link to="/about">About</router-link>
-  </div>
+  <nav>
+    <div class="container">
+      <label class="logo">LOGO</label>
+      <ul id="navItems">
+        <li>
+          <router-link to="/">HOME</router-link>
+        </li>
+        <li>
+          <router-link to="/about">About</router-link>
+        </li>
+        <li>
+          <router-link to="/reservation">Reservation</router-link>
+        </li>
+        <li>
+          <router-link to="/contact">Contact</router-link>
+        </li>
+        <li>
+          <router-link to="/team">Team</router-link>
+        </li>
+      </ul>
+      <label id="icon">
+        <span @click="toggleNavbar">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="28"
+            height="28"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            v-if="!toggled"
+          >
+            <line x1="3" y1="12" x2="21" y2="12" />
+            <line x1="3" y1="6" x2="21" y2="6" />
+            <line x1="3" y1="18" x2="21" y2="18" />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="28"
+            height="28"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            v-else
+          >
+            <line x1="18" y1="6" x2="6" y2="18" />
+            <line x1="6" y1="6" x2="18" y2="18" />
+          </svg>
+        </span>
+      </label>
+    </div>
+  </nav>
 </template>
 <script>
 export default {
-  name: "Navbar"
+  name: "Navbar",
+  data() {
+    return {
+      toggled: 0
+    };
+  },
+  methods: {
+    toggleNavbar() {
+      let x = document.querySelector("#navItems");
+      x.classList.toggle("active");
+      this.toggled = !this.toggled;
+    }
+  }
 };
 </script>
 <style lang="scss" scoped>
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+nav {
+  height: 80px;
+  width: 100%;
+  background: #6c4633;
+  ul {
+    float: right;
+    margin-right: 40px;
   }
-  a.router-link-exact-active {
-    color: #42b983;
+  li {
+    display: inline-block;
+    margin: 0 8px;
+    line-height: 80px;
+  }
+  a {
+    color: white;
+    font-size: 18px;
+    padding: 7px 10px;
+    text-decoration: none;
+    text-transform: uppercase;
+    border-radius: 3px;
+    border: 1px solid transparent;
+  }
+  a.router-link-exact-active,
+  a:hover {
+    transition: 0.5s;
+    border: 1px solid white;
+  }
+  #icon {
+    color: white;
+    font-size: 30px;
+    float: right;
+    margin-right: 40px;
+    line-height: 80px;
+    cursor: pointer;
+    display: none;
+  }
+  label.logo {
+    float: left;
+    font-size: 35px;
+    font-weight: bold;
+    padding: 0 100px;
+    line-height: 80px;
+    color: white;
+  }
+}
+
+@media (max-width: 1048px) {
+  label.logo {
+    font-size: 32px;
+    padding-left: 60px;
+  }
+  nav ul {
+    margin-right: 20px;
+  }
+  nav a {
+    font-size: 17px;
+  }
+}
+@media (max-width: 909px) {
+  nav {
+    #icon {
+      display: block;
+    }
+    ul {
+      position: fixed;
+      width: 100%;
+      height: 100vh;
+      background: #391919;
+      top: 80px;
+      left: -100%;
+      text-align: center;
+      transition: all 0.5s;
+    }
+    li {
+      display: block;
+      margin: 50px 0;
+      line-height: 30px;
+    }
+    a {
+      font-size: 20px;
+    }
+    a.active,
+    a:hover {
+      border: none;
+      color: #3498db;
+    }
+    ul.active {
+      left: 0;
+    }
   }
 }
 </style>
