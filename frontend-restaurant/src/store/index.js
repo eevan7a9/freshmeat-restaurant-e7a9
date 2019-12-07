592d9fdc02e0c02f6da752faa158f0d706e018fe
eevan7a9 <e7a9@protonmail.com>
2019-12-07 18:10:35 +0900

    added restaurant info

--- frontend-restaurant/src/store/index.js
+++ frontend-restaurant/src/store/index.js
@@ -1,14 +1,20 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Restaurant from '@/assets/restaurant.json';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    signature_dish: []
   },
   mutations: {
+    setSignatureDish: (state, value) => state.signature_dish = value
   },
   actions: {
+    getSignatureDish: () => {
+
+    }
   },
   modules: {
   }
