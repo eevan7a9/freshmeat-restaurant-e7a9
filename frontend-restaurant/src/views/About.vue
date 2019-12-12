4a310c7958b9eee521f2c6e44fba0d7642542d74
eevan7a9 <e7a9@protonmail.com>
2019-12-12 18:26:08 +0900

    added about page our story section

--- frontend-restaurant/src/views/About.vue
+++ frontend-restaurant/src/views/About.vue
@@ -1,5 +1,36 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about-wrapper">
+    <div class="about-content">
+      <div class="container">
+        <OurStory />
+      </div>
+    </div>
+    <footer id="footer">
+      <div class="container">
+        <Footer />
+      </div>
+    </footer>
   </div>
 </template>
+<script>
+import OurStory from "@/components/about/OurStory.vue";
+import Footer from "@/components/Footer.vue";
+export default {
+  components: {
+    OurStory,
+    Footer
+  }
+};
+</script>
+<style lang="scss" scoped>
+$mainColor: #a8890f;
+.about-wrapper {
+  min-height: 100vh;
+  .about-content {
+    min-height: 100vh;
+  }
+  #footer {
+    background: $mainColor;
+  }
+}
+</style>
