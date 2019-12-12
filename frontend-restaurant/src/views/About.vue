819eb2433cee86207f1c29573a8814e644a89ea7
eevan7a9 <e7a9@protonmail.com>
2019-12-12 21:25:58 +0900

    added motto

--- frontend-restaurant/src/views/About.vue
+++ frontend-restaurant/src/views/About.vue
@@ -4,6 +4,9 @@
       <section class="container">
         <OurStory />
       </section>
+      <section class="motto">
+        <h1>"We built our reputation by taking care of our customers, with healthy foods."</h1>
+      </section>
       <section id="whyus">
         <WhyUs />
       </section>
@@ -33,6 +36,25 @@ $mainColor: #a8890f;
   min-height: 100vh;
   .about-content {
     min-height: 100vh;
+    .motto {
+      height: 350px;
+      background-image: url("~@/assets/images/bg-restaurant.jpg");
+      background-size: cover;
+      background-position: center;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      h1 {
+        font-weight: 600;
+        font-size: 2.5rem;
+        color: aliceblue;
+        font-family: "Anton", sans-serif;
+        padding: 1rem;
+        text-shadow: 2px 0 #221d1d, 0 1px #827373, 3.5px 3px #443f3c,
+          2px 3px #201f1e;
+        text-align: left;
+      }
+    }
   }
   #footer {
     background: $mainColor;
